ace27ab5bac227222a3dad3b5ae7a06ee74af2a7
puresite23 <103734921+serviatorem@users.noreply.github.com>
2023-10-27 12:10:56 +0300

    feat: add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import TheHeader from '~/components/header/TheHeader.vue'
+</script>
+
+<template>
+  <div class="layout-default">
+    <TheHeader />
+    <main class="main">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.layout-default {
+  width: 100dvw;
+  height: 100dvh;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  background: $background;
+}
+.main{
+  padding: 30px 0;
+  width: 100%;
+}
+</style>
