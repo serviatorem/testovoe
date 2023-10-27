d7e2b51e7f8ef55e9bc925c7293756ae8896eff2
puresite23 <103734921+serviatorem@users.noreply.github.com>
2023-10-27 12:08:08 +0300

    feat: add base tab component

---
+++ components/base/BaseTab.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <button class="button text">
+    <slot />
+  </button>
+</template>
+
+<style scoped lang="scss">
+  .button{
+    outline: none;
+    padding: adpval(5, 10) adpval(10,15);
+    border: 1px solid $lightGrey;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color, color .2s linear;
+    &:hover{
+      background: $violet;
+      color:$lightGrey;
+    }
+  }
+</style>
