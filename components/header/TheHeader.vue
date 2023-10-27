0e3ea1bf933509141207ff6e00a12c7cc572c388
puresite23 <103734921+serviatorem@users.noreply.github.com>
2023-10-27 12:08:20 +0300

    feat: add header

---
+++ components/header/TheHeader.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header class="header">
+    <div class="header__title-wrapper">
+        <h2 class="header__title h2">Отчет Посещений</h2>
+    </div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+  .header{
+    padding: 20px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    -webkit-box-shadow: 0px -5px 5px -5px $shadow inset;
+    -moz-box-shadow: 0px -5px 5px -5px $shadow inset;
+    box-shadow: 0px -5px 5px -5px $shadow inset;
+  }
+
+</style>
