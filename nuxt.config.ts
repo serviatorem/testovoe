c0bd4367dfb59cf3fe379dca1d1b9b8a21b0db17
puresite23 <103734921+serviatorem@users.noreply.github.com>
2023-10-27 12:06:02 +0300

    chore: add config and package

---
+++ nuxt.config.ts
@@ -0,0 +1,41 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    modules: [
+        '@pinia/nuxt',
+        '@nuxtjs/apollo',
+        '@nuxtjs/google-fonts',
+        '@vee-validate/nuxt'
+    ],
+    devtools: {
+      enabled: true
+    },
+    vue: {
+        defineModel: true
+    },
+    components: {
+        pathPrefix: false
+    },
+    apollo: {
+        clients: {
+            default: {
+                httpEndpoint: 'http://test.order.mnogo.menu/graphql/'
+            }
+        },
+    },
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: '@use "@/assets/css/_usage.scss" as *;'
+                }
+            }
+        }
+    },
+    googleFonts: {
+        families:{
+            Roboto:true
+        },
+        subsets:'cyrillic'
+    },
+    css: ['assets/css/reset.scss', 'assets/css/_globals.scss'],
+})
