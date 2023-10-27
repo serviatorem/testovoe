7a5e2b98be8ff583300adad0a23d243877c155a5
puresite23 <103734921+serviatorem@users.noreply.github.com>
2023-10-27 12:09:51 +0300

    feat: add screenIndex component

---
+++ screens/index/ScreenIndex.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import SectionIndexSettings from "~/components/section/SectionIndexSettings.vue";
+import {useMetricStore} from "~/store/store";
+import {storeToRefs} from "pinia";
+import SectionIndexTable from "~/components/section/SectionIndexTable.vue";
+
+const store = useMetricStore()
+const {table} = storeToRefs(store)
+</script>
+
+<template>
+  <section class="index-screen container">
+    <SectionIndexSettings/>
+    <SectionIndexTable/>
+  </section>
+</template>
+
+<style scoped lang="scss">
+.index-screen {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: adpval(80, 120);
+}
+</style>
