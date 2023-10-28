565055ad3be967018435d4301d52cd79f7c55904
puresite23 <103734921+serviatorem@users.noreply.github.com>
2023-10-28 11:01:51 +0300

    feat: add index page

--- pages/index.vue
+++ pages/index.vue
@@ -4,26 +4,6 @@ import ScreenIndex from "~/screens/index/ScreenIndex.vue";
 definePageMeta({
   layout:'default'
 })
-const query = gql`
-  query GetMetrics($input: GetMetricsInput!) {
-      metrics(input: $input) {
-        browser
-        device
-        duration
-        id
-        isNew
-        visitTime
-      }
-    }
-`
-const variables:IVariables = {
-  input: {
-    dateStart: '2023-10-19',
-    dateEnd: '2023-10-19'
-  }
-}
-const { data } = await useAsyncQuery(query,variables);
-console.log(data.value)
 </script>
 
 <template>
