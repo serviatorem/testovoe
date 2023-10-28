75d423abc104904f2195f707bba96861d46e14f1
puresite23 <103734921+serviatorem@users.noreply.github.com>
2023-10-28 11:02:12 +0300

    feat: add screen index

--- screens/index/ScreenIndex.vue
+++ screens/index/ScreenIndex.vue
@@ -2,16 +2,18 @@
 import SectionIndexSettings from "~/components/section/SectionIndexSettings.vue";
 import {useMetricStore} from "~/store/store";
 import {storeToRefs} from "pinia";
-import SectionIndexTable from "~/components/section/SectionIndexTable.vue";
+import SectionIndexTableGeneral from "~/components/section/SectionIndexTableGeneral.vue";
+import SectionIndexTableDevice from "~/components/section/SectionIndexTableDevice.vue";
 
 const store = useMetricStore()
-const {table} = storeToRefs(store)
+const { table, general } = storeToRefs(store)
 </script>
 
 <template>
   <section class="index-screen container">
-    <SectionIndexSettings/>
-    <SectionIndexTable/>
+    <SectionIndexSettings />
+    <SectionIndexTableGeneral v-if="table && general"/>
+    <SectionIndexTableDevice v-if="table && !general"/>
   </section>
 </template>
 
