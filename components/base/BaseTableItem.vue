8ce2f81c41f03349a792a2fcbb7794b40ebc3091
puresite23 <103734921+serviatorem@users.noreply.github.com>
2023-10-28 10:50:23 +0300

    feat: add base component for table

---
+++ components/base/BaseTableItem.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const props = defineProps<{
+  text?:string | number | boolean,
+  border?:'right' | 'left' | 'bottom' | 'top'
+}>()
+</script>
+
+<template>
+  <div class="tableItem" :class="props.border">
+    <p class="tableItem__text text" v-if="props.text">{{ props.text }}</p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .tableItem{
+    display: flex;
+    padding: 10px;
+    align-items: center;
+    justify-content: space-between;
+    border: 1px solid $violet;
+  }
+  .right{
+    border-right: none;
+  }
+  .left{
+    border-left: none;
+  }
+  .top{
+    border-top: none;
+  }
+  .bottom{
+    border-bottom: none;
+  }
+</style>
