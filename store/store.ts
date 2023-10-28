76b95ab4a714d9da57a99be59c776cd89498776d
puresite23 <103734921+serviatorem@users.noreply.github.com>
2023-10-28 22:23:29 +0300

    fix: fix validation

--- store/store.ts
+++ store/store.ts
@@ -2,9 +2,10 @@ import {defineStore} from 'pinia'
 import type {IVariables} from "~/interfaces/IVariables";
 import {useAPI} from "~/API/useAPI";
 import type {IMetric} from "~/interfaces/IMetric";
+
 export const useMetricStore = defineStore('metric', () => {
     const dateMax = ref<string>(new Date().toJSON().slice(0, 10));
-    const dateStart = ref<string>(dateMax.value.slice(0,8) + '01');
+    const dateStart = ref<string>(dateMax.value.slice(0, 8) + '01');
     const dateEnd = ref<string>(dateMax.value);
     const useAPIStore = useAPI();
     const variables = ref<IVariables>({
@@ -13,58 +14,74 @@ export const useMetricStore = defineStore('metric', () => {
             dateEnd: ''
         }
     })
-    async function getMetrics(){
+
+    async function getMetrics() {
         variables.value.input.dateStart = dateStart.value
         variables.value.input.dateEnd = dateEnd.value
         const data = await useAPIStore.getMetrics(variables.value).then(data => data);
-        return data.error.value ? 'неправильная дата': data.data.value.metrics
+        if (!data.error.value) {
+            return data.data.value.metrics
+        }
     }
-    async function getDefaultMetric(){
-        dateStart.value = dateMax.value.slice(0,8) + '01'
+
+    async function getDefaultMetric() {
+        dateStart.value = dateMax.value.slice(0, 8) + '01'
         dateEnd.value = dateMax.value
         return await getMetrics()
     }
-    async function validateMetric(metricNewStore:[IMetric] | string){
-        if (typeof metricNewStore === "string"){
+
+    async function validateMetric(metricNewStore: [IMetric] | undefined) {
+        if (!metricNewStore) {
             const metric = await getDefaultMetric()
-          return {
-              metric:metric,
-              errorText:metricNewStore
-          }
-        }else{
-          return {
-              metric:metricNewStore,
-              errorText:''
-          }
+            return {
+                metric: metric,
+                errorText: 'Неправильная дата'
+            }
+        } else {
+            return {
+                metric: metricNewStore,
+                errorText: ''
+            }
         }
     }
-    async function newDate(newDateStart:string,newDateEnd:string){
+
+    async function newDate(newDateStart: string, newDateEnd: string) {
         dateStart.value = newDateStart
         dateEnd.value = newDateEnd
     }
-    function getMetricDeviceByDate(data:[IMetric], device:string,date:string){
-        return data.filter((item)=> item.device === device && item.visitTime.slice(0,10) === date).length
+
+    function getMetricDeviceByDate(data: [IMetric], device: string, date: string) {
+        return data.filter((item) => item.device === device && item.visitTime.slice(0, 10) === date).length
     }
-    function getMetricAllDevice(data:[IMetric],device:string){
+
+    function getMetricAllDevice(data: [IMetric], device: string) {
         return data.filter((item) => item.device === device).length
     }
-    function getMetricDate(data:[IMetric]){
-        const allDate = new Set();
-        data.forEach((item)=>{ allDate.add(item.visitTime.slice(0,10))})
+
+    function getMetricDate(data: [IMetric]) {
+        const allDate: Set<string> = new Set();
+        data.forEach((item) => {
+            allDate.add(item.visitTime.slice(0, 10))
+        })
         return allDate
     }
-    function getMetricUserByDate(data:[IMetric],date:string){
-        return data.filter((item) => item.visitTime.slice(0,10) === date).length
+
+    function getMetricUserByDate(data: [IMetric], date: string) {
+        return data.filter((item) => item.visitTime.slice(0, 10) === date).length
     }
-    function getMetricAllUser(data:[IMetric]){
+
+    function getMetricAllUser(data: [IMetric]) {
         return data.length
     }
-    function getMetricNewUserByDate(data:[IMetric],date:string){
-        return data.filter((item) => item.visitTime.slice(0,10) === date && item.isNew).length
+
+    function getMetricNewUserByDate(data: [IMetric], date: string) {
+        return data.filter((item) => item.visitTime.slice(0, 10) === date && item.isNew).length
     }
-    function getMetricAllNewUser(data:[IMetric]){
-        return data.filter((item)=>item.isNew).length
+
+    function getMetricAllNewUser(data: [IMetric]) {
+        return data.filter((item) => item.isNew).length
     }
+
     return {
         dateMax,
         dateStart,
@@ -79,6 +96,6 @@ export const useMetricStore = defineStore('metric', () => {
         getMetricUserByDate,
         getMetricAllUser,
         getMetricNewUserByDate,
-        getMetricAllNewUser
+        getMetricAllNewUser,
     }
 })
