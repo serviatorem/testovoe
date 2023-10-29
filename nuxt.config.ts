4438509fab53cef0dfc96c136a90b4be1a2dc566
puresite23 <103734921+serviatorem@users.noreply.github.com>
2023-10-29 14:58:15 +0300

    fix: fix problem htttps and http v5 (apollo no-cors)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,7 +21,7 @@ export default defineNuxtConfig({
                 httpEndpoint: 'http://test.order.mnogo.menu/graphql/',
                 httpLinkOptions: {
                     fetchOptions: {
-                        mode: 'cors'
+                        mode: 'no-cors'
                     }
                 }
             }
